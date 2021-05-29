c9e5db1ed0edc46417d8d6eaaf3b259c0e0f039b
ramvdy <ramvaidya@live.com>
2021-05-30 01:24:50 +0530

    added 2 countries

--- India.py
+++ India.py
@@ -2,5 +2,9 @@ country = input("enter your country name")
 
 if country == "India":
     print("India is popularly called as subcontinent")
+elif country == "USA":
+    print("USA is popularly called America or US or States")
+elif country == "Australia"
+    print(country + " is also a continent")
 else:
     print("your country " + country + " is not a subcontinent")
