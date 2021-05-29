c6e1b7122c6f18ebb332fefd7d6c99ddc823d922
ramvdy <ramvaidya@live.com>
2021-05-30 01:37:18 +0530

    added turkey

--- India.py
+++ India.py
@@ -6,5 +6,7 @@ elif country == "USA":
     print("USA is popularly called America or US or States")
 elif country == "Australia"
     print(country + " is also a continent")
+elif country == "Turkey"
+    print(country + " is also a transcontinental country")
 else:
     print("your country " + country + " is not a subcontinent")
