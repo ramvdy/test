4455124f5a5902cb48c0d4af95def3e34eb58bdc
ramvdy <ramvaidya@live.com>
2021-05-29 20:14:19 +0530

    revert earlier

--- India.py
+++ India.py
@@ -4,4 +4,3 @@ if country == "India":
     print("India is popularly called as subcontinent")
 else:
     print("your country " + country + " is not a subcontinent")
-    print("Since India is great")
