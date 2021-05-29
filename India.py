5be52221cfb2578e0b019e9b44757c8321dfa3e4
ramvdy <ramvaidya@live.com>
2021-05-30 02:04:19 +0530

    older

--- India.py
+++ India.py
@@ -2,15 +2,6 @@ country = input("enter your country name")
 
 if country == "India":
     print("India is popularly called as subcontinent")
-<<<<<<< HEAD
     print("Even Pakistan, Srilanka, Bangladesh and Nepal are considered as Indian subcontinent also south Asian countries")
-=======
-elif country == "USA":
-    print("USA is popularly called America or US or States")
-elif country == "Australia":
-    print(country + " is also a continent")
-elif country == "Turkey":
-    print(country + " is also a transcontinental country")
->>>>>>> countries
 else:
     print("your country " + country + " is not a subcontinent")
