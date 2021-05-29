1e8adf394ddcfe34b7eafb1e7529a7c28853ecd9
ramvdy <ramvaidya@live.com>
2021-05-30 02:05:38 +0530

    corrected errors

--- India.py
+++ India.py
@@ -4,9 +4,9 @@ if country == "India":
     print("India is popularly called as subcontinent")
 elif country == "USA":
     print("USA is popularly called America or US or States")
-elif country == "Australia"
+elif country == "Australia":
     print(country + " is also a continent")
-elif country == "Turkey"
+elif country == "Turkey":
     print(country + " is also a transcontinental country")
 else:
     print("your country " + country + " is not a subcontinent")
