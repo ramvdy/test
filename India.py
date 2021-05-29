bb2c8547026db522347d0f220a833e0cc094f1a8
ramvdy <ramvaidya@live.com>
2021-05-30 01:39:13 +0530

    added other subcontinent countries

--- India.py
+++ India.py
@@ -2,5 +2,6 @@ country = input("enter your country name")
 
 if country == "India":
     print("India is popularly called as subcontinent")
+    print("Even Pakistan, Srilanka, Bangladesh and Nepal are considered as Indian subcontinent also south Asian countries")
 else:
     print("your country " + country + " is not a subcontinent")
