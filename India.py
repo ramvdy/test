a158969cfcf29370719b8ce4755dfccb31dd053b
ramvdy <ramvaidya@live.com>
2021-05-29 19:53:53 +0530

    changes for version git revert commited changes

--- India.py
+++ India.py
@@ -3,4 +3,4 @@ country = input("enter your country name")
 if country == "India":
     print("India is popularly called as subcontinent")
 else:
-    print("your country " + country + " is not a subcontinent")
+    print(country + " is not a subcontinent")
