8efffeadf1f4e36960e63ef822b7d610a2e9de13
ramvdy <ramvaidya@live.com>
2021-05-29 19:33:21 +0530

    Country input is added

--- India.py
+++ India.py
@@ -1,1 +1,6 @@
-print("India is popularly called as subcontinent")
+country = input("enter your country name")
+
+if country == "India":
+    print("India is popularly called as subcontinent")
+else:
+    print("your country " + country + " is not a subcontinent")
